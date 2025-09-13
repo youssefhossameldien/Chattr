27926720520dd1596e6e972d0178d977de457f7e
youssef <siredwardspike@gmail.com>
2025-09-13 18:14:00 +0300

    Deployment related modifications

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
+const __dirname = path.resolve();
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.send(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 app.listen(PORT, () => console.log("server running on port " + PORT));
