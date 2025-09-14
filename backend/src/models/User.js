e2e5bd79f466dd8e6e53ff9ce4b9f9445f20a045
youssef <siredwardspike@gmail.com>
2025-09-14 14:52:35 +0300

    User Schema in MongoDb

---
+++ backend/src/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+const User = mongoose.model("User", userSchema);
+export default User;
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,7 @@
 import express from "express";
+import { signup } from "../controllers/auth.controller.js";
 const router = express.Router();
-router.get("/signup", (req, res) => {
-  res.send("Signup endpoint");
-});
+router.post("/signup", signup);
 router.get("/signin", (req, res) => {
   res.send("Signin endpoint");
 });
