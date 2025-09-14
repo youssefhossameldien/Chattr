7954239943ede91f7668bd2b5db80375fd3162d5
youssef <siredwardspike@gmail.com>
2025-09-14 13:21:07 +0300

    Implementing DB connections

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,13 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import path from "path";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 const __dirname = path.resolve();
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
@@ -17,4 +19,7 @@ if (process.env.NODE_ENV === "production") {
     res.send(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
-app.listen(PORT, () => console.log("server running on port " + PORT));
+app.listen(PORT, () => {
+  console.log("server running on port " + PORT);
+  connectDB();
+});
